04d9211fde63909c9755623603ed551ff07f14c3
Rica Zhang <ricagz@yahoo.com>
2015-11-10 11:16:47 -0500

    added basic user page and fb login

--- account.php
+++ account.php
@@ -8,6 +8,42 @@
     </head>
 
     <body>
+    	<div class="account-module" id="user-profile">
+    		<img src="images/placeholder-user.png" id="user-image"/>
+    		<div id="user-info">
+    			<ul>
+    				<li><h2>username</h2></li>
+    				<li><strong>profile created:</strong> November 1st, 2015</li>
+    				<li><strong>number of designs:</strong> 3</li>
+    				<li><strong>about me:</strong> I like to design calendars.</li>
+    				<li><strong>personal website:</strong> https://www.google.com/</li>
+    			</ul>
+    		</div>
+    	</div><!-- end user-profile -->
+
+		<div class="account-module" id="user-designs">
+    		<h2>designs</h2>
+    		<div>
+    			<img src="images/placeholder-design.svg"/>
+    			<img src="images/placeholder-design.svg"/>
+    			<img src="images/placeholder-design.svg"/>
+    		</div>
+    	</div><!--end designs-->
+
+    	<div class="account-module" id="user-settings">
+    		<h2>settings</h2>
+    		<input type="checkbox" id="emails-checkbox" name="emails-checkbox"><label for="emails-checkbox"> receive emails from the gylt team</label>
+    	</div><!-- end user-settings -->
+
+    	<div class="account-module" id="navigation">
+    		<h2>navigate</h2>
+    		<a href="workspace.php">return to my workspace</a>
+    	</div><!--end nav-->
+
+    	
+
+    	
+
 
     	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-alpha1/jquery.min.js"></script>
 
