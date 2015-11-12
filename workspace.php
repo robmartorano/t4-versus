8681134badfd32a92f3e29ce250e38f0ad1bb5cb
Rica Zhang <ricagz@yahoo.com>
2015-11-12 00:51:51 -0500

    Added design components, jscolor

--- workspace.php
+++ workspace.php
@@ -37,14 +37,14 @@
                     <label class="panel-section-title" for="add-components-checkbox"><i></i>Add Components</label>
                     <div class="panel-section">
                         <div class="add-item">
-                            <h5>Blank Box</h5>
+                            <h4>Blank Box</h4>
                             <input type="text" id="dimension-x-box"/>
                             x
                             <input type="text" id="dimension-y-box"/>
                             <button id="add-rectangle">Add Box</button>
                         </div>
                         <div class="add-item">
-                            <h5>List Box</h5>
+                            <h4>List Box</h4>
                             <input type="text" id="list-length"/> &#35; lines
                             <button id="add-list">Add List</button>
                         </div>
@@ -58,7 +58,30 @@
                     <input type="checkbox" id="design-components-checkbox">
                     <label class="panel-section-title" for="design-components-checkbox"><i></i>Design Components</label>
                     <div class="panel-section">
-                        <p>Something</p>
+                        <div class="design-attributes">Background Color: <input class="jscolor" value="FFFFFF"></div>
+                        <div class="design-attributes">Border Width: 
+                            <select>
+                                <option>1px</option>
+                                <option>2px</option>
+                                <option>3px</option>
+                            </select>
+                        </div>
+                        <div class="design-attributes">Border Color: <input class="jscolor" value="B5B5B5"></div>
+                        <div class="design-attributes">Font Family: 
+                            <select>
+                                <option>Avenir</option>
+                                <option>Helvetica</option>
+                                <option>Times New Roman</option>
+                                <option>Garamond</option>
+                            </select>
+                        </div>
+                        <div class="design-attributes">Font Size: 
+                            <select>
+                                <option>0.75</option>
+                                <option>1</option>
+                                <option>1.25</option>
+                            </select>
+                        </div>
                     </div><!--end panel-section-->
                 </li><!--end design-components-->
             </ul>
@@ -68,6 +91,7 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-alpha1/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.gridster/0.5.6/jquery.gridster.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/less.js/2.5.3/less.min.js"></script>
+        <script src="scripts/jscolor-2.0.4/jscolor.min.js"></script>
 
         <script src="scripts/app.js"></script>
         <script src="scripts/gridster.js"></script>
